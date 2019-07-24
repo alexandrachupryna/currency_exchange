0a6af3dd9be16e3a52f2f67e9d4c4309b7a3d14e
alexandrachupryna <alexandrachuprina93@gmail.com>
2019-07-24 18:51:00 +0300

    Updates

---
+++ precache-manifest.ca07756f8a4cb0f90d9d5aeeb16b939c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0aec143a078d5e60bc734dfd59ce047",
+    "url": "/currency_exchange/index.html"
+  },
+  {
+    "revision": "313c32610de8ed6576d4",
+    "url": "/currency_exchange/static/css/main.4ab468d1.chunk.css"
+  },
+  {
+    "revision": "36bd98f93c72c8f60790",
+    "url": "/currency_exchange/static/js/2.9015a205.chunk.js"
+  },
+  {
+    "revision": "313c32610de8ed6576d4",
+    "url": "/currency_exchange/static/js/main.94c48715.chunk.js"
+  },
+  {
+    "revision": "562a65e69f09da467b54",
+    "url": "/currency_exchange/static/js/runtime~main.b61b9fd0.js"
+  }
+]);
